7a43c0a401d61bf9e07ef32b9a41fbb88e2bcced
Divyansh Rastogi <54467001+divyanshrastogi51@users.noreply.github.com>
2020-02-09 12:11:21 +0530

    Update tr1ts3.c

--- tr1ts3.c
+++ tr1ts3.c
@@ -1,240 +1,153 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-#include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <ctype.h>
+# include <string.h>
 
-struct node
-{
-    int data;
-    struct node *next;
-} ;
-struct node *top;
+# define LPAR '('
+# define RPAR ')'
+# define PLUS '+'
+# define MULT '*'
+# define MOD '%'
+# define SUB '-'
+# define DIV '/'
+# define FINISH '\0'
+# define INT '0'
+# define MAX 100
 
+char stack[MAX];
+int top=-1;
 struct token
 {
     char kind;
     int value;
 };
-
-#define LPAR '('
-#define RPAR ')'
-#define PLUS '+'
-#define MULT '*'
-#define MOD '%'
-#define FINISH '\0'
-#define INT '0'
-
-char infix[100];
 char expr[100];
-char temp[2];
-int where = 0;
-
-void skipWhite();
-void skipDigits();
-struct token getNextToken();
-
-void push(int);
-int pop();
+int where=0;
+void push(char);
+char pop();
+char peek();
 int isEmpty();
-int isfull(struct node *);
-int peek();
-void initstack();
-int incoming_priority(char);
-int in_stack_priority(char);
-
-void main()
-{
-    struct token token;
-    char ch;
-    printf("Input expression: ");
-    fgets(infix, 99, stdin);
-    initstack();
-    token = getNextToken();
-    while (token.kind != FINISH)
-    {
-        if (token.kind == INT)
-        {
-            sprintf(temp, "%d", token.value);
-            strcat(expr, temp);
-            strcat(expr, " ");
-        }
-
-        else
-        {
-            if (token.kind == '(')
-                push(token.kind);
-            else if (token.kind == ')')
-            {
-                ch = peek();
-                while (!isEmpty() && ch != '(')
-                {
-                    ch = pop();
-                    sprintf(temp, "%c", ch);
-                    strcat(expr, temp);
-                    strcat(expr, " ");
-                    ch = peek();
-                }
-                ch = pop();
-            }
-            else
-            {
-                if (isEmpty())
-                    push(token.kind);
-                else
-                {
-                    ch = peek();
-                    if (ch == '(' || incoming_priority(token.kind) > in_stack_priority(ch))
-                    {
-                        push(token.kind);
-                    }
-                    else
-                    {
-                        while (!isEmpty() && ch != '(' && incoming_priority(token.kind) <= in_stack_priority(ch))
-                        {
-                            ch = pop();
-                            sprintf(temp, "%c", ch);
-                            strcat(expr, temp);
-                            strcat(expr, " ");
-                            if (!isEmpty())
-                                ch = peek();
-                        }
-                        push(token.kind);
-                    }
-                }
-            }
-        }
-        token = getNextToken();
-    }
-    while (!isEmpty())
-    {
-        sprintf(temp, "%c", pop());
-        strcat(expr, temp);
-        strcat(expr, " ");
-    }
-    printf("%s\n", expr);
-}
-
-void skipWhite()
-{
-    while (isspace(infix[where]))
-        where++;
-}
-
+int isFull();
+void initStack();
+int prior(char);
 void skipDigits()
 {
-    while (isdigit(infix[where]))
+    while(isdigit(expr[where]))
         where++;
 }
-
+void skipWhite()
+{
+    while(isspace(expr[where]))
+        where++;
+}
 struct token getNextToken()
 {
     struct token token;
     skipWhite();
-
-    if (isdigit(infix[where]))
+    if(isdigit(expr[where]))
     {
-        sscanf(&infix[where], "%d", &token.value);
-        token.kind = INT;
+        sscanf(&expr[where],"%d",&token.value);
+        token.kind='0';
         skipDigits();
     }
+    else if(expr[where]=='\0')
+        token.kind=FINISH;
     else
     {
-        token.kind = infix[where];
+        token.kind=expr[where];
         where++;
     }
     return token;
 }
-
-void push(int item)
+void main()
 {
-    struct node *temp;
-    temp = (struct node *)malloc(sizeof(struct node));
-    if (!isfull(temp))
+    printf("Enter expression: ");
+    gets(expr);
+    char ch;
+    struct token token;
+    int l=strlen(expr);
+    expr[l]=RPAR;
+    expr[l+1]='\0';//to go with algorithm
+    push(LPAR);
+    do
     {
-        temp->data = item;
-        temp->next = top;
-        top = temp;
-    }
+        token=getNextToken();
+        if(token.kind==LPAR)
+            push(LPAR);
+        else if(token.kind==INT)
+            printf("%d ",token.value);
+        else if(token.kind==RPAR)
+        {
+            while(peek()!=LPAR)
+            {
+                ch=pop();
+                printf("%c ",ch);
+
+            }
+            pop();
+        }
+        else if(token.kind!=FINISH)
+        {
+            if(peek()==LPAR)
+                push(token.kind);
+            else
+            {
+                while(prior(peek())>prior(token.kind))
+                {
+                    ch=pop();
+                    printf("%c ",ch);
+                }
+                push(token.kind);
+            }
+        }
+    }while(token.kind!=FINISH);
+
 }
-
-int pop()
+void push(char val)
 {
-    int item;
-    if (!isEmpty())
-    {
-        struct node *temp;
-        temp = top;
-        item = temp->data;
-        top = temp->next;
-        free(temp);
-        return item;
-    }
+    top=top+1;
+    stack[top]=val;
+}
+char pop()
+{
+    char val=stack[top];
+    top=top-1;
+    return val;
+}
+char peek()
+{
+    int a=isEmpty();
+    if(a==1)
+        printf("Stack is empty\n");
     else
-    {
-        printf("WRONG POSTFIX ENTERED!!!\n");
-        exit(0);
-    }
+        return stack[top];
 }
-
 int isEmpty()
 {
-    if (top == NULL)
+    if(top==-1)
         return 1;
     else
         return 0;
 }
-
-int isfull(struct node *temp)
+int isFull()
 {
-    if (temp == NULL)
+    if(top==MAX-1)
         return 1;
     else
         return 0;
 }
-
-void initstack()
+void initStack()
 {
-    top = NULL;
+    top=-1;
 }
-
-int peek()
+int prior(char ch)
 {
-    if (!isEmpty())
-        return top->data;
-    else
-    {
-        printf("THE STACK IS EMPTY!!!\n");
-        exit(0);
-    }
-}
-
-int incoming_priority(char ch)
-{
-    if (ch == '+' || ch == '-')
-        return 1;
-    else if (ch == '*' || ch == '/' || ch == '%')
+    if(ch=='*'||ch=='/'||ch=='%')
         return 2;
-    else if (ch == '^')
-        return 3;
+    else if(ch=='+'||ch=='-')
+        return 1;
     else
-    {
-        printf("-->UNKNOWN CHARACTER ENTERED!!!\n");
-        exit(0);
-    }
+        return -1;
+
 }
 
-int in_stack_priority(char ch)
-{
-    if (ch == '+' || ch == '-')
-        return 1;
-    else if (ch == '*' || ch == '/' || ch == '%')
-        return 2;
-    else if (ch == '^')
-        return 4;
-    else
-    {
-        printf("\nUNKNOWN CHARACTER ENTERED!!!\n");
-        exit(0);
-    }
-}
